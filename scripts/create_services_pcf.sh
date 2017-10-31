bb25c210a78df92a59863360be798eef320bdfb0
Rick Ross <rross@pivotal.io>
2017-10-31 10:07:13 -0400

    Refactoring to work with SCS 1.4

--- scripts/create_services_pcf.sh
+++ scripts/create_services_pcf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cf cs p-mysql 100mb fortunes-db
-cf cs p-config-server standard config-service -c '{ "git": { "uri": "https://github.com/mstine/config-repo" }}' 
+cf cs p-config-server standard config-service -c '{ "git": { "uri": "https://github.com/Pivotal-Field-Engineering/fortune-teller-demo-config-repo.git" }}' 
 cf cs p-service-registry standard service-registry
 cf cs p-circuit-breaker-dashboard standard circuit-breaker
