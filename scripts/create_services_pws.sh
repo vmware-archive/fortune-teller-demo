c4a7b6caa1e0be0d3329b8097e7832ddbbdd60c4
Rick Ross <rross@pivotal.io>
2017-11-02 14:14:05 -0400

    Added script for PWS

---
+++ scripts/create_services_pws.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cf cs cleardb spark fortunes-db
+cf cs p-config-server standard config-service -c '{ "git": { "uri": "https://github.com/Pivotal-Field-Engineering/fortune-teller-demo-config-repo.git" }}' 
+cf cs p-service-registry standard service-registry
+cf cs p-circuit-breaker-dashboard standard circuit-breaker
